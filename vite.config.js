c5a8a29eb7612151f5a53894faeaeb6d38db91b8
Julien Doutre <jul.doutre@gmail.com>
2025-04-05 22:38:48 +0200

    wasm vite support

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
+import wasm from 'vite-plugin-wasm';
+import topLevelAwait from 'vite-plugin-top-level-await';
 
 export default defineConfig({
 	plugins: [
 		sveltekit(),
 		tailwindcss(),
+		wasm(),
+		topLevelAwait(),
 	]
 });
