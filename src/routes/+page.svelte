949202f71ec97dabe2f8be8f6722598d714befbf
Julien Doutre <jul.doutre@gmail.com>
2025-04-05 23:25:32 +0200

    add main page description

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,42 @@
 <script>
-    import { Heading, P } from "flowbite-svelte";
+    import { Heading, P, A, Hr } from "flowbite-svelte";
 </script>
 
-<Heading class="p-8" tag="h1" customSize="text-3xl"
+<Heading class="p-8" tag="h1"
     >Web Assembly Reverse Engineering Challenge</Heading
 >
 
 <P class="px-8 py-4">
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-    tempor incididunt ut labore et dolore magna aliqua. Amet cursus sit amet
-    dictum sit. Quis enim lobortis scelerisque fermentum dui faucibus in. Mattis
-    vulputate enim nulla aliquet porttitor lacus luctus accumsan. Ipsum dolor
-    sit amet consectetur adipiscing elit. Euismod nisi porta lorem mollis
-    aliquam ut porttitor. Tortor consequat id porta nibh. Tortor condimentum
-    lacinia quis vel eros donec ac odio. Elementum sagittis vitae et leo duis ut
-    diam quam nulla. Vel turpis nunc eget lorem.
+    This website contains hacking challenges involving reverse engineering of <A
+        href="https://webassembly.org/">Web Assembly</A
+    > programs.
 </P>
+
+<P class="px-8 py-4"
+    >Levels are increasingly more difficult. Each one exposes a single function
+    callable by Javascript: <code>hash()</code>. It returns the <A
+        href="https://en.wikipedia.org/wiki/SHA-2">sha256</A
+    > sum of a flag. The goal is to retrieve the flag's original value. All flags
+    follow the <code>flag{"{"}...{"}"}</code> format. You can easily check if your
+    guess is correct as you have access to the answer's hash.
+</P>
+
+<Hr />
+
+<Heading class="p-8" tag="h3">Frequently asked questions</Heading>
+
+<P class="px-8 py-4" weight="bold">Can I publish answers?</P>
+<P class="px-8 py-4">Sure, I don't mind if you do!</P>
+
+<P class="px-8 py-4" weight="bold">I found a bug and I'd like to report it</P>
+<P class="px-8 py-4"
+    >Feel free to <A href="https://github.com/juliendoutre/warec/issues"
+        >open an issue</A
+    > on the GitHub repository hosting this website source code 🙏</P
+>
+
+<P class="px-8 py-4" weight="bold">I'd like to propose a new challenge!</P>
+<P class="px-8 py-4"
+    >I definitely accept ideas for new levels! Please send me an email at
+    jul.doutre@gmail.com.</P
+>
