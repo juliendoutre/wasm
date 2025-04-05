4638479aa1b57ccc557e938ad07b50d23b5b678d
Julien Doutre <jul.doutre@gmail.com>
2025-04-05 20:09:51 +0200

    fix build
    Fix build

--- svelte.config.js
+++ svelte.config.js
@@ -6,12 +6,12 @@ const config = {
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
-			fallback: 'index.html',
+			fallback: undefined,
 			precompress: false,
 			strict: true
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			base: process.argv.includes('dev') ? '' : '/warec'
 		},
 	}
 };
